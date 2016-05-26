0ea9fe7482d0395c2b56816092b4515f499cbe83
Martin Ždila <m.zdila@gmail.com>
2016-05-26 08:01:09 +0200

    code cleanup, added some comments, support for MTK3333

--- sketch_gps.ino
+++ sketch_gps.ino
@@ -1,10 +1,22 @@
-#include <SPI.h>
+// NOTE: to support GNSS you must edit libraries/TinyGPSPlus/TinyGPS++.cpp and change these definitions
+// #define _GPRMCterm   "GPRMC"
+// #define _GPGGAterm   "GPGGA"
+// You should also add there support for GNRMC to be parsed as GPRMC because Mediatek outputs GPRMC until the fix
+//
+// HW used:
+// - Arduino Pro Mini (3.3V)
+// - USB Li-Ion charger
+// - Li-Ion battery connected to components; all components must be operable at Li-Ion voltage 3.6V-3.7V (mostly using voltage stabilizer)
+// - GPS NMEA module with serial interface (connected to TX / RX via jumpers to be able to reprogram Arduino)
+// - optionally 3V GPS backup battery if not embedded in GPS module already
+// - SPI SD card module
+// - LED with resistor on pin 4
+// - speaker on pin 2
+// - power switch connecting output of USB charger to VCC
 
-//#include <AltSoftSerial.h>
-// #include <SD.h>
+#include <SPI.h>
 #include <SdFat.h>
 #include <TinyGPS++.h>
-//#include <SoftwareSerial.h>
 
 #undef DEBUG
 
@@ -16,12 +28,8 @@
 #define LOGW(str)
 #endif
 
-//AltSoftSerial mySerial;
-//SoftwareSerial bt(4,5);
 TinyGPSPlus gps;
 boolean cardOk = false;
-boolean btn = false;
-boolean off = false;
 File dataFile;
 
 uint8_t month, day, hour, minute, second;
@@ -47,21 +55,34 @@ void getNewFilename(char* buf, int size) {
 
 char *configs[] = {
   // only RMC and GGA are supported by TinyGPS++
+
+// NOTE: This is for Ublox M8N
+//  "$PUBX,41,1,0007,0003,38400,0*20",
+//  "$PUBX,40,GSV,0,0,0,0,0,0*59", // disable GSV
+//  "$PUBX,40,GLL,0,0,0,0,0,0*5C" // disable GLL
+//  "$PUBX,40,VTG,0,0,0,0,0,0*5E" // disable VTG
+//  "$PUBX,40,GSA,0,0,0,0,0,0*4E" // disable GSA
   
-//  "$PUBX,41,1,0007,0003,38400,0*20\r\n",
-  "$PUBX,40,GSV,0,0,0,0,0,0*59\r\n", // disable GSV
-  "$PUBX,40,GLL,0,0,0,0,0,0*5C\r\n" // disable GLL
-  "$PUBX,40,VTG,0,0,0,0,0,0*5E\r\n" // disable VTG
-  "$PUBX,40,GSA,0,0,0,0,0,0*4E\r\n" // disable GSA
-  
-  
-//  "$PMTK301,2*2E", // WAAS
-//  "$PMTK313,1*2E", // enable SBAS
+
+// NOTE: This is for Mediatek (MT333x)
+  "$PMTK301,2*2E", // WAAS
+  "$PMTK313,1*2E", // enable SBAS
 ////  "$PMTK220,250*29", // 4Hz
-//  "$PMTK220,1000*1F", // 1Hz
-//  "$PMTK314,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29", // RMC, GSA, GGA
-//  "$PMTK397,0*23", // no speed threshold
+  "$PMTK220,1000*1F", // 1Hz
+  "$PMTK314,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29", // RMC, GSA, GGA
+  "$PMTK397,0*23", // no speed threshold
 };
+
+  // $PMTK397,0*23 no speed threshold
+  // $PMTK314,0,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*19
+  // $PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29 // GPRMC
+  // $PMTK314,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29 // GPGGA
+  // $PMTK251,4800*14 // $PMTK251,9600*17 // $PMTK251,19200*22 // $PMTK251,38400*27
+  // $PMTK220,1000*1F // 1Hz
+  // $PMTK220,200*2C // 5Hz
+  // $PMTK220,100*2F // 10Hz
+  // $PMTK104*37 // cold restart
+
 
 void setup() {
   // pinMode(6, INPUT); // button for changing baud rates
@@ -71,26 +92,9 @@ void setup() {
   pinMode(2, OUTPUT); // SPKR
   tone(2, 1055, 200);
 
-
   Serial.begin(9600);
     
-//  bt.begin(9600);
-
   LOG("INIT");
-
-  // set the data rate for the SoftwareSerial port
-  
-  // $PMTK397,0*23 no speed threshold
-  // $PMTK314,0,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*19
-  // $PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29 // GPRMC
-  // $PMTK314,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29 // GPGGA
-  // $PMTK251,4800*14 // $PMTK251,9600*17 // $PMTK251,19200*22 // $PMTK251,38400*27
-  // $PMTK220,1000*1F // 1Hz
-  // $PMTK220,200*2C // 5Hz
-  // $PMTK220,100*2F // 10Hz
-  
-  // Serial.print("$PMTK104*37\r\n"); // reset
-
 
   cardOk = SD.begin(10); // , SPI_HALF_SPEED
   if (!cardOk) {
@@ -123,19 +127,6 @@ void openDataFile() {
 }
 
 void loop() {
-//  if (digitalRead(6) && !btn) {
-//    btn = true;
-//    LOG("BUTTON");
-//    off = true;
-//    if (dataFile) {
-//      dataFile.flush();
-//    }
-//  }
-//  
-//  if (!digitalRead(6) && btn) {
-//    btn = false;
-//  }
-
   if (cardOk && Serial.available()) {
     char c = Serial.read();
     LOGW(c);
@@ -143,18 +134,18 @@ void loop() {
     
     unsigned long m = millis();
     
-    if (dataFile && !off) {
+    if (dataFile) {
       dataFile.write(c);
     
       //we are ok to lost some data, but save write power
       if (c == '\n' && m - time > 30000) {
         time = m;
 
+// some debug stuff
 //        char buf[20];
 //        dtostrf((double) hdop, 10, 3, buf);
 //        dataFile.write(buf);
 //        dataFile.write('\n');
-
 //        dataFile.println(readVcc(), DEC);
 //        dataFile.flush();
         
@@ -163,10 +154,10 @@ void loop() {
         }
       }
     }
-    
+
     if (cmd < sizeof(configs)) {
       if (m > 2000 + cmd * 500) {
-        Serial.print(configs[cmd++]);
+        Serial.println(configs[cmd++]);
         if (cmd == 1) {
           //Serial.begin(38400);
         }
@@ -211,52 +202,6 @@ void loop() {
  
 }
 
-byte getUbxAck(byte *msgID) {
-  byte ckA = 0, ckB = 0;
-  byte incoming_char;
-  boolean headerReceived = false;
-  unsigned long ackWait = millis();
-  byte ackPacket[10] = {0xB5, 0x62, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-  int i = 0;
-
-  for (;;) {
-    if (Serial.available()) {
-      incoming_char = Serial.read();
-      if (incoming_char == ackPacket[i]) {
-        i++;
-      } else if (i > 2) {
-        ackPacket[i] = incoming_char;
-        i++;
-      }
-    }
-    if (i > 9) {
-      break;
-    }
-    if (millis() - ackWait > 1500) {
-      LOG("ACK Timeout");
-      return 5;
-    }
-    if (i == 4 && ackPacket[3] == 0x00) {
-      LOG("NAK Received");
-      return 1;
-    }
-  }
-
-  for (i = 2; i < 8 ;i++) {
-    ckA = ckA + ackPacket[i];
-    ckB = ckB + ckA;
-  }
-
-  if (msgID[0] == ackPacket[6] && msgID[1] == ackPacket[7] && ckA == ackPacket[8] && ckB == ackPacket[9]) {
-    LOG("Success");
-    return 10;
-  }
-
-  LOG("ACK Checksum Failure");
-  delay(1000);
-  return 1;
-}
-
 void dateTime(uint16_t* date, uint16_t* time) {
   // return date using FAT_DATE macro to format fields
   *date = FAT_DATE(year, month, day);
@@ -265,15 +210,62 @@ void dateTime(uint16_t* date, uint16_t* time) {
   *time = FAT_TIME(hour, minute, second);
 }
 
-long readVcc() {
-  long result;
-  // Read 1.1V reference against AVcc
-  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  delay(2); // Wait for Vref to settle
-  ADCSRA |= _BV(ADSC); // Convert
-  while (bit_is_set(ADCSRA,ADSC));
-  result = ADCL;
-  result |= ADCH<<8;
-  result = 1126400L / result; // Back-calculate AVcc in mV
-  return result;
-}
+//byte getUbxAck(byte *msgID) {
+//  byte ckA = 0, ckB = 0;
+//  byte incoming_char;
+//  boolean headerReceived = false;
+//  unsigned long ackWait = millis();
+//  byte ackPacket[10] = {0xB5, 0x62, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+//  int i = 0;
+//
+//  for (;;) {
+//    if (Serial.available()) {
+//      incoming_char = Serial.read();
+//      if (incoming_char == ackPacket[i]) {
+//        i++;
+//      } else if (i > 2) {
+//        ackPacket[i] = incoming_char;
+//        i++;
+//      }
+//    }
+//    if (i > 9) {
+//      break;
+//    }
+//    if (millis() - ackWait > 1500) {
+//      LOG("ACK Timeout");
+//      return 5;
+//    }
+//    if (i == 4 && ackPacket[3] == 0x00) {
+//      LOG("NAK Received");
+//      return 1;
+//    }
+//  }
+//
+//  for (i = 2; i < 8 ;i++) {
+//    ckA = ckA + ackPacket[i];
+//    ckB = ckB + ckA;
+//  }
+//
+//  if (msgID[0] == ackPacket[6] && msgID[1] == ackPacket[7] && ckA == ackPacket[8] && ckB == ackPacket[9]) {
+//    LOG("Success");
+//    return 10;
+//  }
+//
+//  LOG("ACK Checksum Failure");
+//  delay(1000);
+//  return 1;
+//}
+
+
+//long readVcc() {
+//  long result;
+//  // Read 1.1V reference against AVcc
+//  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+//  delay(2); // Wait for Vref to settle
+//  ADCSRA |= _BV(ADSC); // Convert
+//  while (bit_is_set(ADCSRA,ADSC));
+//  result = ADCL;
+//  result |= ADCH<<8;
+//  result = 1126400L / result; // Back-calculate AVcc in mV
+//  return result;
+//}
